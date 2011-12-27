8b7e2ae9782fcab827d5ac59e8e6a8efe5ac95e5
kazuyukitanimura <kazuyukitanimura@gmail.com>
2011-12-26 19:59:42 -0800

    multicore

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var fs = require('fs');
 var cluster = require('cluster');
 var numCPUs = require('os').cpus().length;
 var WebSocketServer = require('websocket').server;
+var net = require('net');
 
 /**
  * ARGV Set
@@ -23,10 +24,19 @@ if (process.argv[2]) { // the first argument
 
 if (cluster.isMaster) {
 
-  // Fork workers.
-  for (var i = 0; i < numCPUs; i++) {
+  // Fork workers, numCPUs - 1
+  for (var i = 1; i < numCPUs; i++) {
     cluster.fork();
   }
+  //var server = net.createServer();
+  //server.listen('./sock');
+  //server.on('connection',function(socket){
+  //  socket.on('data', function(data){
+  //    console.log(data);
+  //  });
+  //  var b = new Buffer([1,2,3]);
+  //  socket.write(b);
+  //});
 
   cluster.on('death', function(worker) {
     console.log('worker ' + worker.pid + ' died. restart...');
@@ -44,10 +54,18 @@ if (cluster.isMaster) {
   });
 
   wsServer.on('request', function(request) {
+    console.log('\nWorker ID: '+process.env.NODE_WORKER_ID);
     var connection = request.accept(null, request.origin);
     connection.on('frame', function(frame) {
       connection.sendFrame(frame);
     });
   });
+
+  //var socket = net.connect('./sock');
+  //var b = new Buffer([3,4,5]);
+  //socket.write(b);
+  //socket.on('data', function(data){
+  //  console.log(data);
+  //});
 }
 
